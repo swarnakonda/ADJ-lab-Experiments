3c94b29ed1f15c12a8d66bf5651bc306c8dcdf87
swarnakonda <50200852+swarnakonda@users.noreply.github.com>
2021-02-06 19:00:58 +0530

    Experiment1

---
+++ Experiment1/src/arraylist/Array.java
@@ -0,0 +1,21 @@
+package arraylist;
+
+import java.util.ArrayList;
+public class Array {
+public static void main(String args[]) {
+ArrayList<String> obj = new ArrayList<String>();
+obj.add("Ajeet");
+obj.add("Harry");
+obj.add("Chaitanya");
+obj.add("Steve");
+obj.add("Anuj");
+System.out.println("Currently the array list has following elements:"+obj);
+obj.add(0, "Rahul");
+obj.add(1, "Justin");
+obj.remove("Chaitanya");
+obj.remove("Harry");
+System.out.println("Current array list is:"+obj);
+obj.remove(1);
+System.out.println("Current array list is:"+obj);
+}
+}
---
+++ Experiment1/src/hashmap/Collec3.java
@@ -0,0 +1,25 @@
+package hashmap;
+
+import java.util.*;
+public class Collec3 {
+public static void main(String args[]) {
+HashMap hm = new HashMap();
+hm.put("Zara", new Double(3434.34));
+hm.put("Mahnaz", new Double(123.22));
+hm.put("Ayan", new Double(1378.00));
+hm.put("Daisy", new Double(99.22));
+hm.put("Qadir", new Double(-19.08));
+Set set = hm.entrySet();
+Iterator i = set.iterator();
+while(i.hasNext()) {
+Map.Entry me = (Map.Entry)i.next();
+System.out.print(me.getKey() + ": ");
+System.out.println(me.getValue());
+}
+System.out.println();
+double balance = ((Double)hm.get("Zara")).doubleValue();
+hm.put("Zara", new Double(balance + 1000));
+System.out.println("Zara's new balance: " +
+hm.get("Zara"));
+}
+}
